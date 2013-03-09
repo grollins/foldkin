94141b422605af5b83f88590197195ee3f90b3b2
Geoff Rollins <grollins@gmail.com>
2013-03-08 17:48:07 -0800

    Implement parameter distribution factory class.

--- foldkin/test/test_param_distribution.py
+++ foldkin/test/test_param_distribution.py
@@ -1,18 +1,23 @@
 import nose.tools
 import numpy
-from foldkin.parameter_set_distribution import ParameterSetDistribution
+from foldkin.parameter_set_distribution import ParamSetDistFactory,\
+                                               ParameterSetDistribution
 from foldkin.coop.coop_model_parameter_set import CoopModelParameterSet
 
 @nose.tools.istest
 def correctly_selects_optimal_parameters():
-    ps_distribution = ParameterSetDistribution()
+    psd_factory = ParamSetDistFactory()
     ps = CoopModelParameterSet()
     ps.update_from_array( numpy.array([1.0, 1.0, 1.0, 1.0]) )
-    ps_distribution.add_parameter_set(ps, 100.)
+    psd_factory.add_parameter_set(ps)
+    psd_factory.add_parameter('score', 100.)
     ps.update_from_array( numpy.array([2.0, 2.0, 2.0, 2.0]) )
-    ps_distribution.add_parameter_set(ps, 1.)
+    psd_factory.add_parameter_set(ps)
+    psd_factory.add_parameter('score', 1.)
     ps.update_from_array( numpy.array([3.0, 3.0, 3.0, 3.0]) )
-    ps_distribution.add_parameter_set(ps, 1000.)
+    psd_factory.add_parameter_set(ps)
+    psd_factory.add_parameter('score', 1000.)
+    ps_distribution = psd_factory.make_psd()
 
     best_ps_dict = ps_distribution.get_best_parameter_set()
     nose.tools.eq_(best_ps_dict['score'], 1.0)
