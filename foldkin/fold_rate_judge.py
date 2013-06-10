52c69ecb995ed1f210b1ff645ed8c40c272044be
Geoff Rollins <grollins@gmail.com>
2013-06-10 16:27:52 -0700

    Implement R-squared judge as an alternative to rms error judge.

--- foldkin/fold_rate_judge.py
+++ foldkin/fold_rate_judge.py
@@ -1,6 +1,9 @@
+import numpy
 from numpy import isnan
 from base.judge import Judge
 from sklearn.metrics import mean_squared_error
+from foldkin.coop.coop_model import CoopModelFactory
+from foldkin.util import convert_T_to_beta
 
 def print_nan_error_msg(target_array, prediction_array):
     error_msg = ""
@@ -48,10 +51,32 @@ class CoopCollectionJudge(Judge):
         return mse, prediction
 
 
+class CoopCollectionR2Judge(Judge):
+    def __init__(self):
+        super(CoopCollectionR2Judge, self).__init__()
+
+    def judge_prediction(self, model, data_predictor, target_data, noisy=False):
+        feature_array = target_data.get_feature()
+        target_array = target_data.get_target()
+        prediction = data_predictor.predict_data(model)
+        prediction_array = prediction.as_array_from_id_list(feature_array)
+
+        if noisy:
+            for i,f in enumerate(feature_array):
+                print f, prediction_array[i], target_array[i]
+
+        error_msg = "%s  %s" % (target_array.shape, prediction_array.shape)
+        assert target_array.shape == prediction_array.shape, error_msg
+        cc = numpy.corrcoef(target_array, prediction_array)[0,1]
+        R2 = cc**2
+        return R2, prediction
+
+
 class TemperatureDependenceJudge(Judge):
     """docstring for TemperatureDependenceJudge"""
     def __init__(self):
         super(TemperatureDependenceJudge, self).__init__()
+        self.mf = CoopModelFactory()
 
     def judge_prediction(self, fold_model, unfold_model, fold_data_predictor,
                          unfold_data_predictor, fold_target_data,
@@ -89,5 +114,6 @@ class TemperatureDependenceJudge(Judge):
         else:
             unfold_mse = 0.0
             unfold_prediction = None
+        score = (fold_mse + unfold_mse)/2.
 
-        return (fold_mse + unfold_mse)/2, fold_prediction, unfold_prediction
+        return score, fold_prediction, unfold_prediction
